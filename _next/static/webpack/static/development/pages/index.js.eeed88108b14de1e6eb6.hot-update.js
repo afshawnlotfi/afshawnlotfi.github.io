ca42b011a9ac8a3d711f874ad93f0bd0d20040d8
Afshawn Lotfi <me@afshawn.org>
2021-06-28 23:57:19 -0400

    first commit

---
+++ _next/static/webpack/static/development/pages/index.js.eeed88108b14de1e6eb6.hot-update.js
@@ -0,0 +1,123 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./website-config.tsx":
+/*!****************************!*\
+  !*** ./website-config.tsx ***!
+  \****************************/
+/*! exports provided: userName, groups, computerName, interests, aboutInfo, researchAndProjects, skillsAndInterests */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "userName", function() { return userName; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "groups", function() { return groups; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "computerName", function() { return computerName; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "interests", function() { return interests; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "aboutInfo", function() { return aboutInfo; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "researchAndProjects", function() { return researchAndProjects; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "skillsAndInterests", function() { return skillsAndInterests; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _material_ui_icons_Description__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @material-ui/icons/Description */ "./node_modules/@material-ui/icons/Description.js");
+/* harmony import */ var _material_ui_icons_Description__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_Description__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _material_ui_icons_GitHub__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @material-ui/icons/GitHub */ "./node_modules/@material-ui/icons/GitHub.js");
+/* harmony import */ var _material_ui_icons_GitHub__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_GitHub__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "/workspaces/website/website-config.tsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var userName = "afshawnlotfi";
+var groups = ["KrossyKey", "Conva", "phonerobot"];
+var computerName = "afshawns-mbp";
+var interests = {
+  "react-native": 232825,
+  "reverse-engineering": 190825
+};
+var aboutInfo = {
+  image: "https://avatars2.githubusercontent.com/u/6283745?s=460&u=304c5e89862646af4a6764e837d86ba261dfc739&v=4",
+  name: "Afshawn Lotfi",
+  bio: "Georgia Tech Alum. Interested in solving big problems!",
+  options: [{
+    name: "Github",
+    url: "https://github.com/afshawnlotfi",
+    svg: __jsx(_material_ui_icons_GitHub__WEBPACK_IMPORTED_MODULE_2___default.a, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 24,
+        columnNumber: 12
+      }
+    })
+  }, {
+    name: "Resume",
+    url: "https://afshawn.org/files/Resume.pdf",
+    svg: __jsx(_material_ui_icons_Description__WEBPACK_IMPORTED_MODULE_1___default.a, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 29,
+        columnNumber: 12
+      }
+    })
+  }]
+};
+var researchAndProjects = {
+  name: "Research & Projects",
+  description: "Here are a list of research and projects that I have been working on",
+  sections: [{
+    name: "Research",
+    command: "./my-research",
+    infos: [{
+      name: "GT DILAC Lab",
+      description: "researching word embeddings of   \n various works of literature throughout history",
+      url: "http://dilab.gatech.edu/"
+    }, {
+      name: "GT CyFI Lab",
+      description: "researched Swift IOS malware and \n creating tools to detect such binaries",
+      url: "https://cyfi.ece.gatech.edu/"
+    }]
+  }, {
+    name: "Projects",
+    command: "./my-projects",
+    infos: [{
+      name: "Operation Dynamo",
+      description: "website for crowd-sourcing   \n medical equipment to combat COVID-19",
+      url: "https://operationdynamo.org/"
+    }, {
+      name: "Conva LLC",
+      description: "app for finding people nearby your  \n location to go on spontaneous adventures with",
+      url: "https://conva.app"
+    }, {
+      name: "KrossyKey",
+      description: "web app for storing passwords,\n notes, and two-factor keys offline and anywhere",
+      url: "https://krossykey.com"
+    }, {
+      name: "Bold Browser",
+      description: "app that streamlines the web     \n browser experience by allowing custom layouts",
+      url: "https://github.com/afshawnlotfi/boldbrowser"
+    }]
+  }]
+};
+var skillsAndInterests = function skillsAndInterests(repoStats) {
+  return {
+    name: "Skills & Interests",
+    sections: [{
+      name: "Skills",
+      description: "a list of some of my skills",
+      command: "./my-skills",
+      stats: repoStats ? repoStats.langs : []
+    }, {
+      name: "Interests",
+      description: "a list of some of my interests",
+      command: "./my-interests",
+      stats: repoStats ? repoStats.topics : []
+    }],
+    description: "Here are some of my skills and stats on my experience in them"
+  };
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.eeed88108b14de1e6eb6.hot-update.js.map
